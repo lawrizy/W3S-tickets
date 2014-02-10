6b652d75f4592f353c38d0a6e5c442adbf5c6b58
DesaedeleerL <shasnape06@gmail.com>
2014-02-10 11:20:09 +0100

    Loging user et locataire + MD5

--- protected/components/UserIdentity.php
+++ protected/components/UserIdentity.php
@@ -5,29 +5,48 @@
  * It contains the authentication method that checks if the provided
  * data can identity the user.
  */
-class UserIdentity extends CUserIdentity
-{
-	/**
-	 * Authenticates a user.
-	 * The example implementation makes sure if the username and password
-	 * are both 'demo'.
-	 * In practical applications, this should be changed to authenticate
-	 * against some persistent user identity storage (e.g. database).
-	 * @return boolean whether authentication succeeds.
-	 */
-	public function authenticate()
-	{
-		$users=array(
-			// username => password
-			'demo'=>'demo',
-			'admin'=>'admin',
-		);
-		if(!isset($users[$this->username]))
-			$this->errorCode=self::ERROR_USERNAME_INVALID;
-		elseif($users[$this->username]!==$this->password)
-			$this->errorCode=self::ERROR_PASSWORD_INVALID;
-		else
-			$this->errorCode=self::ERROR_NONE;
-		return !$this->errorCode;
-	}
+class UserIdentity extends CUserIdentity {
+
+    /**
+     * Authenticates a user.
+     * The example implementation makes sure if the username and password
+     * are both 'demo'.
+     * In practical applications, this should be changed to authenticate
+     * against some persistent user identity storage (e.g. database).
+     * @return boolean whether authentication succeeds.
+     */
+    private $_id;
+    private $_table;
+
+    public function authenticate() {
+
+        //   $record = Locataire::model()->findByAttributes(array('email' => $this->username));
+        if (($record = Locataire::model()->findByAttributes(array('email' => $this->username))) !== null) {
+            if ($record->password !== md5($this->password))
+                $this->errorCode = self::ERROR_PASSWORD_INVALID;
+            else {
+                $this->_id = $record->id_locataire;
+                $this->errorCode = self::ERROR_NONE;
+            }
+            return !$this->errorCode;
+        } elseif (($record = User::model()->findByAttributes(array('email' => $this->username))) !== NULL) {
+
+            if ($record->password !== md5($this->password))
+                $this->errorCode = self::ERROR_PASSWORD_INVALID;
+            else {
+                $this->_id = $record->id_user;
+                $this->errorCode = self::ERROR_NONE;
+            }
+            return !$this->errorCode;
+        }
+    }
+
+    public function getId() {
+        return $this->_id;
+    }
+
+    public function getStatut() {
+        return $this->_table;
+    }
+
 }
