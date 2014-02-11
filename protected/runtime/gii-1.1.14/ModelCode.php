e62e12f8038d62113092ce33dcbced2202f87284
Ridounet <developper02@gmail.com>
2014-02-11 10:17:47 +0100

    add CRUD from new tables (Entreprise, Batiment, ...)

--- protected/runtime/gii-1.1.14/ModelCode.php
+++ protected/runtime/gii-1.1.14/ModelCode.php
@@ -1,15 +1,4 @@
-<<<<<<< HEAD
-<?php
-return array (
-  'template' => 'default',
-  'connectionId' => 'db',
-  'tablePrefix' => 'w3sys_',
-  'modelPath' => 'application.models',
-  'baseClass' => 'CActiveRecord',
-  'buildRelations' => '1',
-  'commentsAsLabels' => '0',
-);
-=======
+
 <?php
 
 return array(
@@ -21,4 +10,3 @@ return array(
     'buildRelations' => '1',
     'commentsAsLabels' => '0',
 );
->>>>>>> 11491a7f40938f72ce5a9d8c131056ced1d02d27
