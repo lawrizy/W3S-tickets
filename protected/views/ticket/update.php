69661a1c5abbf7f30bce0ce93d12e66d208295c3
Oscuro87 <capelle.e@gmail.com>
2014-02-13 11:03:02 +0100

    Le locataire n'a plus accès aux menus d'administration

--- protected/views/ticket/update.php
+++ protected/views/ticket/update.php
@@ -2,18 +2,22 @@
 /* @var $this TicketController */
 /* @var $model Ticket */
 
-$this->breadcrumbs = array(
-    'Tickets' => array('index'),
-    $model->id_ticket => array('view', 'id' => $model->id_ticket),
-    'Update',
-);
 
-$this->menu = array(
-    array('label' => 'List Ticket', 'url' => array('index')),
-    array('label' => 'Create Ticket', 'url' => array('create')),
-    array('label' => 'View Ticket', 'url' => array('view', 'id' => $model->id_ticket)),
-    array('label' => 'Manage Ticket', 'url' => array('admin')),
-);
+if(Yii::app()->session['Utilisateur'] === 'User')
+{
+    $this->breadcrumbs = array(
+        'Tickets' => array('index'),
+        $model->id_ticket => array('view', 'id' => $model->id_ticket),
+        'Update',
+    );
+
+    $this->menu = array(
+        array('label' => 'List Ticket', 'url' => array('index')),
+        array('label' => 'Create Ticket', 'url' => array('create')),
+        array('label' => 'View Ticket', 'url' => array('view', 'id' => $model->id_ticket)),
+        array('label' => 'Manage Ticket', 'url' => array('admin')),
+    );
+}
 ?>
 
 <h1>Update Ticket <?php echo $model->id_ticket; ?></h1>
