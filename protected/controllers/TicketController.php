9598976be5bd59f45b0eae9796be05d06df6953f
DesaedeleerL <shasnape06@gmail.com>
2014-02-20 14:23:41 +0100

    Mise a jour vue login vue ticket

--- protected/controllers/TicketController.php
+++ protected/controllers/TicketController.php
@@ -109,7 +109,6 @@ class TicketController extends Controller {
                 $histo->fk_user = $logged['id_user'];
                 $histo->save(FALSE);
                 Yii::trace('apres save de l\'historique', 'cron');
-                Yii::app()->session['NouveauTicket']='nouveau';
                 $this->redirect(array('view', 'id' => $oldmodel['id_ticket']));
             } catch (CDbException $ex) {
                 Yii::trace('dans catch', 'cron');
@@ -179,6 +178,7 @@ class TicketController extends Controller {
                 $histo->fk_user = $model['fk_user'];
                 $histo->save(FALSE);
                 // Si tout s'est bien passé, on redirige vers la page view
+                Yii::app()->session['NouveauTicket']='nouveau';
                 $this->redirect(array('view', 'id' => $model->id_ticket));
             } catch (CDbException $e) {
 
