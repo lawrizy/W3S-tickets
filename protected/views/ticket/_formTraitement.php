926a59ad5c01c0e51ae9b3b57d21156e4830024d
Ridounet <developper02@gmail.com>
2014-02-21 18:14:22 +0100

    re-adapt folder 'scripts et docs', add 'DOC Ouverture ticket' to principal doc, comment to view traitement and add datepicker to astuce yii

--- protected/views/ticket/_formTraitement.php
+++ protected/views/ticket/_formTraitement.php
@@ -4,7 +4,7 @@
 /* @var $form CActiveForm */
 ?>
 <script>
-    $(function() {
+    $(function() { // fonction JQuery permettant d'afficher le datePicker
         $("#anim").change(function() {
             $("#Ticket[date_intervention]").datepicker("option", "showAnim", "show");
         });
@@ -18,57 +18,51 @@
     <?php echo $form->errorSummary($model); ?>
     <?php
 // Affichage de la sélection des entreprises
-    echo $form->labelEx($model, 'fk_entreprise');
+    echo '<label>Entreprise <span class="required">*</span></label>';
     echo $form->dropDownList($model, 'fk_entreprise', array('' => '', CHtml::listData(Entreprise::model()->findAll(), 'id_entreprise', 'nom')));
-// Yii::trace(CVarDumper::dumpAsString($theData), 'cron');
+        // Affiche la liste des entreprises que l'on peut assigner à ce ticket
     echo $form->error($model, 'fk_entreprise');
     ?>
 
     <div class="row">
+        <label>Date d'intervention <span class="required">*</span></label>
         <?php
-        echo $form->labelEx($model, 'date_intervention');
-        $this->widget('zii.widgets.jui.CJuiDatePicker', array(
+        $this->widget('zii.widgets.jui.CJuiDatePicker', array( // Widget permettant d'afficher un datePicker
             'name' => 'Ticket[date_intervention]',
             'id' => 'Ticket_date_intervention',
-            // additional javascript options for the date picker plugin
+            // Le datePicker utilise du javascript, voir la function plus haut
             'options' => array(
-                'dateFormat' => 'yy/mm/dd',
-                'showAnim' => 'show', //'slide','fold','slideDown','fadeIn','blind','bounce','clip','drop'
-                'changeMonth' => true,
-                'changeYear' => true,
-                'minDate' => date('Y/m/d'), // minimum date
-            ),
-            'htmlOptions' => array(
-                'style' => 'height:20px;',
-            ),
-        ));
+                'dateFormat' => 'yy/mm/dd', // Permet de définir le format d'affichage de la date
+                'showAnim' => 'show', // Affiche une animation à l'affichage du datePicker, choix entre tout ce qu'il y a en commentaire en-dessous
+                //'slide','fold','slideDown','fadeIn','blind','bounce','clip','drop'
+                
+                'changeMonth' => true, // Affiche une comboBox permettant de sélectionner un mois directement
+                'changeYear' => true, // Affiche une comboBox permettant de sélectionner une année directement
+                'minDate' => date('Y/m/d'), // Toutes les dates avant celle-ci ne seront pas sélectionnables
+                'htmlOptions' => array(
+                    'readonly' => 'readonly',
+                    'style' => 'height:20px;',
+                    'size' => 43),
+            ))
+        );
         ?>
-
-
     </div>
+</div>
 
-    <div class="row">
-        <!-- Div pour la PRIORITE -->
-        <!-- Champs caché -->
-        <!-- TODO Priorité -->
-    </div>
+<div class="row buttons">
+    <?php
+    $this->widget('zii.widgets.jui.CJuiButton', array(
+        'buttonType' => 'submit', // Type de bouton
+        'name' => 'traitement', // L'action à lancer (ne pas oublier les rules dans le controleur)
+        'caption' => 'Passer en Traitement', // Le texte à afficher sur le bouton
+    ));
+    ?>
 
-
-    <div class="row buttons">
-        <?php
-        $this->widget('zii.widgets.jui.CJuiButton', array(
-            'buttonType' => 'submit',
-            'name' => 'traitement',
-            'caption' => 'Passer en InProgress',
-        ));
-//---------------------------------
-        ?>
-
-    </div>
-    <br />
-    <br />
+</div>
+<br />
+<br />
 
 
-    <?php $this->endWidget(); ?>
+<?php $this->endWidget(); ?>
 
 </div><!-- form -->
