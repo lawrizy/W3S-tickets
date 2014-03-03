3f228460493a909e0bb6862e3a4172c3ba6a7061
DesaedeleerL <shasnape06@gmail.com>
2014-03-03 17:36:07 +0100

    mise a jour fin de l'internationalisation db + commentaire

--- protected/controllers/Translate.php
+++ protected/controllers/Translate.php
@@ -2,17 +2,17 @@
 
 class Translate {
     
-    public static function tradPetit($txt) {
-        $result = TradPetit::model()->findByPk($txt);
+    public static function tradPetit($txt) {      //traduit les phrases de maximum 32 caractères
+        $result = TradPetit::model()->findByPk($txt); 
         return $result[Yii::app()->session['_lang']];
     }
     
-    public static function tradMoyen($txt) {
+    public static function tradMoyen($txt) {   //traduit les phrases de maximum 64 caractères
         $result = TradMoyen::model()->findByPk($txt);
         return $result[Yii::app()->session['_lang']];
     }
     
-    public static function tradGrand($txt) {
+    public static function tradGrand($txt) {  // traduit les phrases de maximum de 128 caractères 
         $result = TradGrand::model()->findByPk($txt);
         return $result[Yii::app()->session['_lang']];
     }
