5384bf710e03712c23755c9223f328b3c7d9499c
DesaedeleerL <shasnape06@gmail.com>
2014-03-06 18:07:20 +0100

    mise a jour pour presentation

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -6,7 +6,6 @@ class SiteController extends Controller {
 
     public static function assignLangue() {  // fonction statique qui permet soit de recuperer la langue soit de remettre la langue de l'application par défaut = en
         if (!empty(Yii::app()->session['_lang'])) { // si il y a une langue 
-
             Yii::app()->language = Yii::app()->session['_lang'];  // la langue de l'application est definie par l'utilisateur
         } else {   // sinon 
             Yii::app()->session['_lang'] = 'en'; // la langue est l'anglais par défaut 
