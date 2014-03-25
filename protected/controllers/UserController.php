e2f9bee9257010437a702ff19b957bcba24cccf0
Ridounet <developper02@gmail.com>
2014-03-25 10:49:11 +0100

    push test

--- protected/controllers/UserController.php
+++ protected/controllers/UserController.php
@@ -15,6 +15,7 @@ class UserController extends Controller {
     const ACTION_ADMIN = 16;
     const ACTION_CHANGEPASSWORD = 32;
 
+    
     /**
      * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
      * using two-column layout. See 'protected/views/layouts/column2.php'.
