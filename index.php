581f94569381144a5c16aed17a9eb870034bff92
Oscuro87 <capelle.e@gmail.com>
2014-04-12 10:49:29 +0200

    Push disable cache soap

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
 // change the following paths if necessary
+ini_set("soap.wsdl_cache_enabled", "0");
 $yii = dirname(__FILE__) . '/yii/framework/yii.php';
 $config = dirname(__FILE__) . '/protected/config/main.php';
 // remove the following lines when in production mode
